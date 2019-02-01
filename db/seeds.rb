4ae33166472fd96ed1dbdb4170164a42d9be1d88
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-01-31 23:39:39 -0800

    Add seed files for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create(title: "Blog number #{blog + 1}",
+               body: 'Lorem ipsum dolor sit amet, consectetur
+                      adipisicing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                      laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur.
+                      Excepteur sint occaecat cupidatat non proident,
+                      sunt in culpa qui officia deserunt mollit anim id
+                      est laborum.')
+end
+
+puts '##### Added 10 blog posts ######'
+
+5.times do |portfol|
+  Portfol.create(title: "portfol #{portfol + 1}",
+                 subtitle: "ruby on rails #{portfol + 1}",
+                 body: 'Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.',
+                  main_image: 'https://via.placeholder.com/350x150',
+                  thumb_image: 'https://via.placeholder.com/200x80')
+end
+
+
+puts '##### 5 Portfolio item has created #####'
+
+7.times do |skill|
+  Skill.create(title: "Skill Number #{skill + 1}",
+               percent_utilize: 30)
+end
+
+
+puts '##### 7 Skill set has been added ######'
