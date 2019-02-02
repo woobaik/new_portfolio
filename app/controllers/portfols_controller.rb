e945aa638d918ef0a88df6c1fc937a09c3f1f75c
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-01 22:13:37 -0800

    Add time stamps on portfolios

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -1,2 +1,44 @@
 class PortfolsController < ApplicationController
+
+    def index
+      @portfols = Portfol.all
+    end
+
+    def new
+      @portfol = Portfol.new
+    end
+
+    def create
+      @portfol = Portfol.new(portfol_params)
+
+      if @portfol.save
+        redirect_to @portfol
+      end
+    end
+
+    def edit
+      @portfol = Portfol.find(params[:id])
+    end
+
+    def update
+      @portfol = Portfol.find(params[:id])
+
+      if @portfol.update
+        redirect_to @portfol
+      end
+    end
+
+    def show
+      @portfol = Portfol.find(params[:id])
+    end
+
+  private
+
+    def portfol_params
+      params.require(:portfol).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
+                                      :thumb_image)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
